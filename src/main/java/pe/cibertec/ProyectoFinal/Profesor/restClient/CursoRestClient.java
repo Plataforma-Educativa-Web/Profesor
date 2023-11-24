65db25894fd77bd88517002076fe05e66f574659
Victor Borja <vborja2424@gmail.com>
2023-11-24 14:45:28 -0500

    Terminado

--- src/main/java/pe/cibertec/ProyectoFinal/Profesor/restClient/CursoRestClient.java
+++ src/main/java/pe/cibertec/ProyectoFinal/Profesor/restClient/CursoRestClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import pe.cibertec.ProyectoFinal.Profesor.entity.Curso;
 
-@FeignClient(value = "curso-service", url = "http://localhost:8085/")
+@FeignClient(value = "curso-service", url = "http://localhost:8083/")
 
 public interface CursoRestClient {
 
